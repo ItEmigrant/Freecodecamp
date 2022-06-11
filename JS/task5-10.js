c243eeb2519b53ed8f58834ff60dbfd6c08fb192
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-11 21:54:30 +0200

    task5-10.js

---
+++ JS/task5-10.js
@@ -0,0 +1,38 @@
+//task5.js
+var myVar = 0;
+var a = 9;
+
+//task6.js
+var myFirstName = "Bohdan";
+var myLastName = "Solomchak";
+
+//task7.js
+// Змініть код лише під цим рядком
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Змініть код лише над цим рядком
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//task8.js
+// Оголошення змінної
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Призначення змінної
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//task9.js
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//task10.js
+const FCC = "freeCodeCamp"; // Змініть цей рядок
+let fact = "is cool!"; // Змініть цей рядок
+fact = "is awesome!";
+console.log(FCC, fact); // Змініть цей рядок
