4e72ba8ca7f11d9f5fa56548065db0d9b95ff6fd
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-12 15:25:21 +0200

    task24-30

---
+++ JS/task24-30.js
@@ -0,0 +1,38 @@
+//task24
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+//task25
+let a1 = 48;
+let b1 = 108;
+let c1 = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+//task26
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+// Змініть цей рядок
+
+//task27
+const myStr1 = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+//task28
+const myStr2 = "FirstLine\n\t\\SecondLine\nThirdLine"; // Змініть цей рядок
+
+//task29
+const myStr3 = "This is the start. " + "This is the end.";// Змініть цей рядок
+
+//task30
+let myStr4 = "This is the first sentence. ";
+myStr += "This is the second sentence."
+
