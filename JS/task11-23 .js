a970f9c720c614122fe8773088fe22c176519f7b
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-12 11:13:47 +0200

    task11-23.js

---
+++ JS/task11-23 .js
@@ -0,0 +1,59 @@
+//task11
+const sum = 10 + 10;
+
+//task12
+const difference = 45 - 33;
+
+//task13
+const product1 = 8 * 10;
+
+//task14
+const quotient = 66 / 33;
+
+//task15
+let myVar1 = 87;
+// Змініть код лише під цим рядком
+myVar++;
+
+//task16
+let myVar = 11;
+
+// Змініть код лише під цим рядком
+myVar--;
+
+//task17
+const ourDecimal = 5.7;
+
+// Змініть код лише під цим рядком
+let myDecimal = 8.8
+
+//task18 
+const product = 2.0 * 2.5;
+
+//task19
+const quotient1 = 4.4 / 2.0; // Змініть цей рядок
+
+//task20
+let remainder = 0;
+remainder = 11 % 3
+
+//task21
+let a = 3;
+let b = 17;
+let c = 12;
+
+//task22
+// Змініть код лише під цим рядком
+a += 12;
+b += 9;
+c += 7;
+
+//task23
+let a1 = 11;
+let b1 = 9;
+let c1 = 3;
+
+// Змініть код лише під цим рядком
+a -= 6;
+b -= 15;
+c -= 1;
