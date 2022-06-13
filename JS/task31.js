96b5fb44c0c14f55775b66f41dc673e40be84801
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-13 14:02:10 +0200

    task31-35.js

---
+++ JS/task31.js
@@ -0,0 +1,40 @@
+//task31
+const myName = "Bogdan";
+const myStr = "My name is" + myName + "and I am well";
+
+//task31
+// Змініть код під цим рядком
+const someAdjective = "cool";
+let myStr1 = "Learning to code is ";
+myStr += someAdjective;
+
+//task32
+// Налаштування
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Змініть код лише під цим рядком
+lastNameLength = lastName.length;
+
+//task33
+// Налаштування
+let firstLetterOfLastName = "";
+const lastName3 = "Lovelace";
+
+// Змініть код лише під цим рядком
+firstLetterOfLastName = lastName[0]; // Змініть цей рядок
+
+//task34
+// Налаштування
+let myStr5 = "Jello World";
+
+// Змініть код лише під цим рядком
+myStr = "Hello World"; // Змініть цей рядок
+// Змініть код лише над цим рядком
+
+//task35
+// Налаштування
+const lastName4 = "Lovelace";
+
+// Змініть код лише під цим рядком
+const thirdLetterOfLastName = lastName[2]; // Змініть цей рядок
