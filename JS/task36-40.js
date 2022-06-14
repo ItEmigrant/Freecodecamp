e70f8052d8871063eecfe5844ff13b02765f5f37
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-14 14:53:51 +0200

    task36-40.js

---
+++ JS/task36-40.js
@@ -0,0 +1,32 @@
+//task36
+// Налаштування
+const lastName = "Lovelace";
+
+// Змініть код лише під цим рядком
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Змініть цей рядок
+
+//task37
+// Налаштування
+const lastName1 = "Lovelace";
+
+// Змініть код лише під цим рядком
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Змініть цей рядок
+
+//task38
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Змініть код лише під цим рядком
+const wordBlanks = "My " + myNoun + " Arnold was a " +
+    myAdjective + " and fast " + myVerb + " and " + myAdverb; // Змініть цей рядок
+// Змініть код лише над цим рядком
+
+//task39 
+// Змініть код лише під цим рядком
+const myArray = ["hello!", 22];
+
+//task40
+// Змініть код лише під цим рядком
+const myArray = [["red, 44"], [555, 5559, "cat"]];
