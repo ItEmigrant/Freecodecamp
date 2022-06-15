da30e1cac994cdce161fbfb33fbca400d761c693
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-15 12:33:48 +0200

    task41-45.js

---
+++ JS/task41-45.js
@@ -0,0 +1,35 @@
+//task41
+const myArray = [50, 60, 70];
+
+var myData = myArray[0];
+
+//task42
+// Налаштування
+const myArray1 = [18, 64, 99];
+
+// Змініть код лише під цим рядком
+myArray[0] = 45;
+
+//task43
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];
+  //task44
+  // Налаштування
+const myArray = [["John", 23], ["cat", 2]];
+
+// Змініть код лише під цим рядком
+myArray.push(["dog", 3]);
+
+//task45
+// Налаштування
+const myArray = [["John", 23], ["cat", 2]];
+
+// Змініть код лише під цим рядком
+var removedFromMyArray = myArray.pop()
+
