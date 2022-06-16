c8aea8a5e2721ea13217a0a82c4ee7ae5306b625
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-16 15:56:57 +0200

    task46-50.js

---
+++ HTML+CSS/task46-50.js
@@ -0,0 +1,32 @@
+//task46
+// Налаштування
+const myArray = [["John", 23], ["dog", 3]];
+
+// Змініть код лише під цим рядком
+const removedFromMyArray = myArray.shift();
+
+//task47
+// Налаштування
+const myArray2 = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Змініть код лише під цим рядком
+myArray.unshift(["Paul", 35]);
+
+//task48
+const myList = [["cake",3], ["cream", 4], ["banan", 5] ,["aplle", 6], ["rose", 9]];
+
+//task49
+function reusableFunction () {
+    console.log("Hi World");
+  }
+  reusableFunction ()
+
+  //task50
+  function functionWithArgs(a, b) {
+
+    console.log(a+b);
+ }
+ 
+ functionWithArgs(1, 2);
+ functionWithArgs(7, 9);
