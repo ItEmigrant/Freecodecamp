9458aa4035a7566528ccb894344027da1d92c2b3
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-19 09:58:23 +0200

    task59-61.js

---
+++ JS/task59-61.js
@@ -0,0 +1,35 @@
+//task59
+function trueOrFalse(wasThatTrue) {
+    // Змініть код лише під цим рядком
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    }
+    return "No, that was false";
+
+    // Змініть код лише над цим рядком
+
+}
+trueOrFalse(true);
+trueOrFalse(false);
+
+//task60
+// Налаштування
+function testEqual(val) {
+    if (val == 12) { // Змініть цей рядок
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+testEqual(10);
+
+//task61
+// Налаштування
+function testStrict(val) {
+    if (val===7) { // Змініть цей рядок
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(10);
