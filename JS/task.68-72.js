8db68c861849efff5eefa9f9717f928043b003a9
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-22 16:29:44 +0200

    task68-75

--- JS/task.68-72.js
+++ JS/task.68-72.js
@@ -76,3 +76,68 @@ function testSize(num) {
   }
   
   testSize(7);
+
+  //task73
+  const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Змініть код лише під цим рядком
+if (strokes == 1) {
+    return names[0]
+  } else if (strokes <= par - 2) {
+    return names[1]
+  } else if (strokes == par - 1) {
+    return names[2]
+  } else if (strokes == par) {
+    return names[3]
+  } else if (strokes == par + 1) {
+    return names[4]
+  } else if (strokes == par + 2) {
+    return names[5]
+  } else if (strokes >= par + 3) {
+    return names[6]
+  }
+
+  return "Change Me";
+  // Змініть код лише над цим рядком
+}
+
+golfScore(5, 4);
+
+//task74
+function caseInSwitch(val) {
+  let answer = "";
+  // Змініть код лише під цим рядком
+switch (val) {
+  case 1: answer = "alpha"; break;
+  case 2: answer = "beta"; break;
+  case 3: answer = "gamma"; break;
+  case 4: answer = "delta"; break;
+}
+
+
+  // Змініть код лише над цим рядком
+  return answer;
+}
+
+caseInSwitch(1);
+
+//task75
+function switchOfStuff(val) {
+  let answer = "";
+  // Змініть код лише під цим рядком
+switch (val) {
+  case "a": answer = "apple"; break; 
+  case "b": answer = "bird"; break; 
+  case "c": answer = "cat"; break; 
+  default: answer =  "stuff";
+    break;
+  }
+
+
+  // Змініть код лише над цим рядком
+  return answer;
+}
+
+switchOfStuff(1);
+
