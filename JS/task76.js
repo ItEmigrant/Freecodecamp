73fba62f75b18517a0cd4a13458dfdb01a4a3eca
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-23 11:07:59 +0200

    task76.js

---
+++ JS/task76.js
@@ -0,0 +1,27 @@
+//task76
+function sequentialSizes(val) {
+    let answer = "";
+    // Змініть код лише під цим рядком
+  switch (val) {
+    case 1: 
+    case 2: 
+    case 3: 
+    answer = "Low"; break; 
+    case 4: 
+    case 5: 
+    case 6: 
+     answer = "Mid"; break; 
+    case 7: 
+    case 8: 
+    case 9:  
+    answer = "High"; break; 
+      result = "1, 2, or 3";
+      break;
+     }
+  
+  
+    // Змініть код лише над цим рядком
+    return answer;
+  }
+  
+  sequentialSizes(1);
