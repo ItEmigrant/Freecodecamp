ff9955418bcf82eaafc42caa708b2f1f1aeae7e5
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-24 13:10:31 +0200

    task81-84.js

---
+++ JS/task81-84.js
@@ -0,0 +1,46 @@
+//task81
+const myDog = {
+    // Змініть код лише під цим рядком
+  "name": "Timon",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["mariana", "Luda", "Alasa"]
+    // Змініть код лише над цим рядком
+  };
+
+  //task82
+  // Налаштування
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Змініть код лише під цим рядком
+  const hatValue = testObj.hat;      // Змініть цей рядок
+  const shirtValue = testObj.shirt;    // Змініть цей рядок
+
+  //task83
+  // Налаштування
+const testObj2 = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Змініть код лише під цим рядком
+  const entreeValue = testObj["an entree"];   // Змініть цей рядок
+  const drinkValue = testObj["the drink"];    // Змініть цей рядок
+
+  //task84
+  // Налаштування
+const testObj1 = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Змініть код лише під цим рядком
+  
+  const playerNumber = 16;  // Змініть цей рядок
+  const player = testObj[playerNumber];   // Змініть цей рядок
