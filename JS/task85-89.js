04c1c1a456b892e87f7e5725115569f76321fe60
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-25 16:04:24 +0200

    task 85-89

--- JS/task85-89.js
+++ JS/task85-89.js
@@ -31,5 +31,36 @@ const myDog = {
   // Змініть код лише під цим рядком
   delete myDog.tails;
 
+  //task88
+  // Налаштування
+function phoneticLookup(val) {
+  let result = "";
+
+  // Змініть код лише під цим рядком
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  };
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
+
+//task89
+
+function checkObj(obj, checkProp) {
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+    return "Not Found";
+  }
+}
+
   
 
