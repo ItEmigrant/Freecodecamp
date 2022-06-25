40fbd62797da31870705db358a85db1165ef1a8c
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-25 15:21:30 +0200

    task85-87.js

---
+++ JS/task85-87.js
@@ -0,0 +1,35 @@
+//task85
+// Налаштування
+const myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+myDog.name = "Happy Coder";
+// Змініть код лише під цим рядком
+
+//task86
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+myDog["bark"] = "Gav";
+
+  //task87
+// Налаштування
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  
+  // Змініть код лише під цим рядком
+  delete myDog.tails;
+
+  
+
