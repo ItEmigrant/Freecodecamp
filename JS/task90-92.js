085a7534edaff5005c9fa2fc2609452ea192c7b2
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-26 21:56:39 +0200

    task90-92.js

---
+++ JS/task90-92.js
@@ -0,0 +1,41 @@
+//task90
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Beau Carnes",
+      "title": "Cereal Man",
+      "release_year": 2003,
+      "formats": [
+        "YouTube video",
+        "Musik"
+      ]
+    }
+  ];
+
+  //task91
+  const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"];
+
+  //task92
+  
