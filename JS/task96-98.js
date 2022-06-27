6c15c93fa33b70a65e40e21f8600731c39f222f9
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-27 23:22:43 +0200

    task96-98.js

---
+++ JS/task96-98.js
@@ -0,0 +1,28 @@
+//task96
+// Налаштування
+const myArray = [];
+for (let i=1; i<=9; i +=2 ){
+  myArray.push(i);
+}
+// Змініть код лише під цим рядком
+console.log (myArray); 
+
+//task97
+// Налаштування
+const myArray3 = [];
+
+// Змініть код лише під цим рядком
+for (let i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
+console.log(myArray);
+
+//task98
+// Налаштування
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+// Змініть код лише під цим рядком
+  for (let i=0; i<myArr.length; i++) {
+    total = total + myArr[i];
+  }
+  console.log (total); 
