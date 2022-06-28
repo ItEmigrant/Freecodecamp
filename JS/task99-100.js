1059651e85a17d0147e2ce0c7676cde901e08b19
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-28 23:40:24 +0200

    task99-100.js

---
+++ JS/task99-100.js
@@ -0,0 +1,30 @@
+//task99
+function multiplyAll(arr) {
+    let product = 1;
+    // Змініть код лише під цим рядком
+  for (var i=0; i<arr.length; i++) {
+    for (var j=0; j<arr[i].length; j++) {
+     product = product * arr[i][j];
+  }
+  }
+    // Змініть код лише над цим рядком
+    return product;
+  }
+  
+  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+  
+  
+//task100
+// Налаштування
+const myArray = [];
+let i = 10;
+
+// Змініть код лише під цим рядком
+
+do {
+  myArray.push(i);
+  i++;
+}
+while (i < 5) 
+
+
