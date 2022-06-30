8e77428cf1abc5103725afac4d668631853e7739
Bogdan Sol <bohdan.jur@gmail.com>
2022-06-30 14:57:49 +0200

    task102-103

---
+++ JS/task102.js
@@ -0,0 +1,18 @@
+//task102
+function randomFraction() {
+
+    // Змініть код лише під цим рядком
+  let n = Math.random(); 
+    return n;
+  
+    // Змініть код лише над цим рядком
+  }
+
+  //task103
+  function randomWholeNum() {
+
+    // Змініть код лише під цим рядком
+    
+  
+    return Math.floor(Math.random() * 10);
+  }
