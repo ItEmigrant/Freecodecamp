05cb7ed5edb75134aee28c87bc1a8f5754402e31
Bogdan Sol <bohdan.jur@gmail.com>
2022-07-06 13:05:52 +0200

    task115-117

---
+++ JS/task115.js
@@ -0,0 +1,38 @@
+//task115
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Змініть код лише під цим рядком
+  
+  const {today, tomorrow} = HIGH_TEMPERATURES;
+  
+  // Змініть код лише над цим рядком
+
+  //task116
+  const HIGH_TEMPERATURES1 = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Змініть код лише під цим рядком
+  const {today:highToday, tomorrow:highTomorrow} = HIGH_TEMPERATURES;
+  
+  
+  // Змініть код лише над цим рядком
+
+  //task117
+  const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+  // Змініть код лише під цим рядком
+  
+  const { today: { low: lowToday, high: highToday1 } } = LOCAL_FORECAST;
+  
+  // Змініть код лише над цим рядком
