d0e132c1fbede72a3fe4d0788ea444aebda1b525
Bogdan Sol <bohdan.jur@gmail.com>
2022-07-08 23:51:04 +0200

    task123.js

---
+++ JS/task123.js
@@ -0,0 +1,13 @@
+//task123
+// Змініть код лише під цим рядком
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+      this.gear = newGear;
+    }
+  };
+  // Змініть код лише над цим рядком
+  bicycle.setGear(3);
+  console.log(bicycle.gear);
+
+
