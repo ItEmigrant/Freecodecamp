55baf1a883f93571e49e13cad980ed4e8ed0e74e
Bogdan Sol <bohdan.jur@gmail.com>
2022-07-11 11:36:09 +0200

    task135 - 136

---
+++ JS/task135.js
@@ -0,0 +1,9 @@
+//task135
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Змініть цей рядок
+
+//task136
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Змініть цей рядок
+let result1 = waldoRegex.test(waldoIsHiding);
