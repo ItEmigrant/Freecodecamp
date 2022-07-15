b06d978408b36b245680ab2d1249b23aa02f5d63
Bogdan Sol <bohdan.jur@gmail.com>
2022-07-15 14:26:13 +0200

    task159-160

---
+++ JS/task159-160.js
@@ -0,0 +1,9 @@
+//task159
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/g; // Змініть цей рядок
+let result = haRegex.test(haStr);
+
+//task160
+let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/g; // Змініть цей рядок
+let result1 = timRegex.test(timStr);
