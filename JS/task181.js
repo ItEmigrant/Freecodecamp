8d3c640afa765ef045945a2575fd8bd0bd03192b
Bogdan Sol <bohdan.jur@gmail.com>
2022-07-21 00:08:17 +0200

    task181

---
+++ JS/task181.js
@@ -0,0 +1,8 @@
+function htmlColorNames(arr) {
+    // Змініть код лише під цим рядком
+  arr.splice(0,2,'DarkSalmon', 'BlanchedAlmond' )
+    // Змініть код лише над цим рядком
+    return arr;
+  }
+  
+  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
