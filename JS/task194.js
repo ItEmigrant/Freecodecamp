ecdbd44e960b66ff326e989abff32b85d6424740
Bohdan Solomchak <bohdan.jur@gmail.com>
2022-10-27 16:38:07 +0200

    task194-196A

---
+++ JS/task194.js
@@ -0,0 +1,32 @@
+// task 194
+function convertCtoF(celsius) {
+    let fahrenheit;
+    fahrenheit = (celsius*9)/5 + 32
+    return fahrenheit;
+  }
+  
+  convertCtoF(30);
+
+  //task195
+  function reverseString(str) {
+    let spliteString = str.split("");
+    let reverseString = spliteString.reverse();
+    let joinString = reverseString.join("");
+ 
+   return joinString;
+ }
+ 
+ reverseString("hello");
+
+ //task196
+ function factorialize(num) {
+    let fakt = 1;
+    for (let i=2; i<=num; i++ ) {
+      
+      fakt = fakt*i;
+    
+    }
+    return fakt;
+  }
+  
+  factorialize(5); 
